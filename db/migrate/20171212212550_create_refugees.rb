de31e327b0ad8c42663462fb832e8c519b60fe8f
Ileana Ordonez <ileana.s.ordonez@gmail.com>
2017-12-12 16:34:20 -0500

    first official commit

---
+++ db/migrate/20171212212550_create_refugees.rb
@@ -0,0 +1,11 @@
+class CreateRefugees < ActiveRecord::Migration[5.1]
+  def change
+    create_table :refugees do |t|
+      t.string :countryOfOrigin
+      t.integer :refugees
+      t.string :countryOfAsylum
+      
+      t.timestamps
+    end
+  end
+end
